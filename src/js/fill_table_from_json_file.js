e5e246a79c8e3ba878ec36f52f895d6a18b55c01
rodrigo bernardo medeiros <rodrimedeiros@gmail.com>
2022-11-03 00:33:07 -0300

    include all needed to update alerts when click in a row option

--- src/js/fill_table_from_json_file.js
+++ src/js/fill_table_from_json_file.js
@@ -4,11 +4,11 @@ var sites = data["sites"];
 
 // get table body to include elements.
 var tableRef = document.getElementById('table').getElementsByTagName('tbody')[0];
+var countSites = 0;
 
 for (let site of sites) {
-    var newRow = tableRef.insertRow(tableRef.rows.length);
-    newRow.innerHTML = `
-    <tr>
+    tableRef.innerHTML += `
+    <tr id="tr-${countSites}">
         <td><strong>${site["Name"]}</strong></td>
         <td>
             <label class="badge badge-success">${site["Alerts"]["high"]["count"]}</label>
@@ -20,10 +20,79 @@ for (let site of sites) {
         <td>${site["Power"]}</td>
     </tr>
     `;
+    countSites += 1;
 }
+
+function getAlertDetails(index) {
+    var siteToGetDetails = sites[index];
+    var alerts = siteToGetDetails['Alerts'];
+    var details = [];
+
+    for (let key in alerts) {
+        var alert = alerts[key];
+
+        if ("details" in alert) {
+            details = [...details, ...alert['details']];
+        }
+    }
+
+    return details;
+}
+
 
 // Add click event to tr elements
 $('#table tbody tr').click(function() {
+    
+    // Get the id of clicked tr element.
+    var trId = $(this).attr('id');
+
+    // Get site index to extract information.
+    var index = trId.split('-')[1];
+
+    var clickedSite = sites[index];
+
+    var baseCard = document.getElementById('base-alert-card');
+    baseCard.innerHTML = `
+    <div class="container">
+        <div class="card">
+            <div class="card-body" id="current-site">
+                <h4 class="card-title">${clickedSite['Name']}</h4>
+            </div>
+            </div>
+        </div>
+    </div>
+    `
+    // Get details from clicked site
+    var details = getAlertDetails(index)
+
+    var currentCart = document.getElementById('current-site');
+
+    if (details.length === 0) {
+        currentCart.innerHTML += `
+        <div class="card text-white bg-secondary">
+            <div class="card-body">
+                <p class="card-text">There is no alerts!</p>
+            </div>
+            </div>
+        </div>
+        <br>
+        `
+    }
+
+    for (let detail of details) {
+        currentCart.innerHTML += `
+        <div class="card text-white bg-info">
+            <div class="card-body">
+                <h4 class="card-title">${detail['metric']}</h4>
+                <p class="card-text">Value: ${detail['value']} ${detail['unit']}</p>
+                <p class="card-text">Time: ${detail['time']}</p>
+            </div>
+            </div>
+        </div>
+        <br>
+        `
+    }
+
     $(this).addClass('bg-success').siblings().removeClass('bg-success');
-    console.log('teste');
+
 });
